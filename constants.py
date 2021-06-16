d9c0d0faa44ee13be8740599272b7261660dcfd0
Ramiro <ramipadro93@gmail.com>
2021-06-16 19:36:10 -0300

    coord_finder finished - connection to firestore db

---
+++ constants.py
@@ -0,0 +1,7 @@
+ARES_ID = "b8b30697-ff7c-4f79-9541-2d999a471fab"
+VEGA_ID = "2f965194-a236-41b9-8807-6bcc8e6aa734"
+TITAN_ID = "78e96ca5-97dc-4eb0-b4bc-73f72da3df7b"
+NOVA_ID = ""
+
+EMAIL = ("ramiropadrog@gmail.com")
+PASSWORD = ("rami38050222")
---
+++ coord_finder.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+import firebase_admin 
+from firebase_admin import credentials
+from firebase_admin import firestore
+import constants
+from pandas import DataFrame
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.support import expected_conditions
+import requests
+import time
+import random
+from selenium import webdriver
+import platform
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import pandas as pd
+import datetime
+import decimal
+from pyvirtualdisplay import Display
+
+cred  = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+display = Display(visible=0, size=(1024, 768))
+display.start()
+if __name__ == "__main__":
+	path = ""
+
+	options = webdriver.ChromeOptions()
+	options.add_argument('--no-sandbox')
+	driver = webdriver.Chrome(options=options)
+	driver.get("https://ogamex.net/#")
+	element = driver.find_element_by_xpath('//*[@id="bodyWrapper"]/footer/div/div[2]/a[2]')
+	element.click()
+	element = driver.find_element_by_name("Email")
+	element.send_keys(constants.EMAIL)
+	element = driver.find_element_by_name("Password")
+	element.send_keys(constants.PASSWORD)
+
+	element = driver.find_element_by_id("btnLogin")
+	element.click()
+	time.sleep(2)
+	driver.get("https://ogamex.net/connect?serverId=" + constants.VEGA_ID) #vega
+	driver.get("https://vega.ogamex.net/galaxy")
+	time.sleep(1)
+
+	#text_file = open("debug", "w")
+	#text_file.write(driver.page_source)
+	#text_file.close()
+	gal = driver.find_element_by_id("galaxyInput")
+	valu = driver.find_element_by_id("systemInput")
+	secs = (float(random.randint(50, 70))/50)
+
+	for i in range(1,6):
+		gal.clear()
+		gal.send_keys(i) #setting galaxy_number in input
+
+		for j in range(1,500):
+			valu.send_keys(Keys.BACKSPACE)
+			valu.send_keys(Keys.BACKSPACE)
+			valu.send_keys(Keys.BACKSPACE)
+
+			time.sleep(secs)
+
+			valu.send_keys(j) #setting system_number in input
+			valu.send_keys(Keys.ENTER)
+
+			time.sleep(secs)
+			pagesource = driver.page_source
+			soup = BeautifulSoup(pagesource, 'html.parser')
+			info = soup.find_all('div', class_="galaxy-col col-player")
+			#moon = soup.find_all('div',class_="galaxy-col col-moon")
+			planet = soup.find_all('div',class_="galaxy-col col-planet-index")
+			vega = soup.find_all('div', class_="galaxy-col col-alliance")
+			for k in range(15): #search every planet
+				# # Extract & Save Timestamp System, Galaxy, Position and PlayerName
+				#galaxy.append(i)
+				#system.append(j)
+				#planet_no.append(planet[k].find('span').get_text())
+				#player.append(info[k].find('span').get_text())
+				#status = info[k].find('span',{'class':['isInactive28 tooltip','isInactive7 tooltip', 'isVacation tooltip', 'isNoob tooltip']})
+				
+				rankCompare = 0
+				r = info[k].find('span').get('data-tooltip-content')
+				if r:
+					r = BeautifulSoup(r, 'html.parser')
+					r = r.find('a', class_="galaxy-shortcut-action")
+					rankCompare = int(r.get_text())
+				
+				
+				if rankCompare <= 20 and rankCompare >= 1:					
+					if info[k].find('span').get_text() != "":
+						print("")
+						print("Galaxy: " + str(i) + " - System: " + str(j) + " - Position: " + planet[k].find('span').get_text())
+						print(f"Mapping took {secs} seconds")
+						db.collection('player_coords').add({'player_name':info[k].find('span').get_text(), 
+						'galaxy_number':i, 
+						'system_number':j, 
+						'position_number': planet[k].find('span').get_text()})
+
+	driver.quit()
+	display.stop()
